e748ce0210a2953c2ab2baca39ddcc1bd198451f
A. Sharma <ankagugu@gmail.com>
2021-04-18 17:54:10 -0700

    searches nearby zipcodes + adding and removing cleaner

--- app.py
+++ app.py
@@ -16,16 +16,65 @@ mongo = PyMongo(app)
 
 regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$' #for sever-side validation of email
 
+
+@app.route("/", methods= ["GET", "POST"])
+def showData():
+    if request.method == "GET":
+        return render_template ("showData.html", people={})
+    else:
+        bloodType = request.form ["bloodType"]
+        zipcode = request.form ["zipcode"].strip()
+        if not zipcode.isnumeric():
+            flash("zipcode must be numeric", "warning")
+            return redirect ("/")
+        #make sure to retain leading zeros in zip when adding and subtracting 1
+        lenZip = len(zipcode)
+        searchZipcodeUP = ( str( int(zipcode) + 1 ) ).zfill(lenZip)
+        searchZipcodeDOWN = ( str( int(zipcode) - 1 ) ).zfill(lenZip)
+        
+        name = ""
+        people =  {}
+        #persons = mongo.db.newrecords.find({"bloodType":bloodType, "zipcode": zipcode})
+        persons = mongo.db.newrecords.aggregate([ {"$match":{"bloodType":bloodType, "zipcode": zipcode}}, {"$sample":{ "size": 10 }} ])
+        #print(persons)
+        for person in persons:
+            if "name" in person:
+                name = person["name"]
+            people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
+            #print (person, people)"""
+
+        if len(people) < 10:
+            persons = mongo.db.newrecords.aggregate([ {"$match":{"bloodType":bloodType, "zipcode": searchZipcodeUP}}, {"$sample":{ "size": 10 - len(people) }} ])
+            for person in persons:
+                if "name" in person:
+                    name = person["name"]
+                people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
+
+        if len(people) < 10:
+            persons = mongo.db.newrecords.aggregate([ {"$match":{"bloodType":bloodType, "zipcode": searchZipcodeDOWN}}, {"$sample":{ "size": 10 - len(people) }} ])
+            for person in persons:
+                if "name" in person:
+                    name = person["name"]
+                people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
+
+        if people == {}:
+            flash("No applicable donors near you", "warning")
+        elif len(people) < 10:
+            zipcodes = zipcode + ", " + searchZipcodeDOWN + ", " + searchZipcodeUP
+            flash("Unable to find 10 donors in "+zipcodes, "warning")
+        return render_template ("showData.html", people = people)
+
+
 @app.route("/add", methods= ["GET", "POST"])
 def add():
     if request.method == "GET":
         return render_template ("add.html")
     else:
-        name = request.form ["name"]
-        phone = request.form ["phone"]
-        zipcode = request.form ["zipcode"]
+        name = request.form ["name"].strip()
+        phone = request.form ["phone"].strip()
+        zipcode = request.form ["zipcode"].strip()
         bloodType = request.form ["bloodType"]
-        email = request.form ["email"]
+        email = request.form ["email"].strip()
         if (phone == "" or zipcode == ""):
             flash("Please fill in all fields", "danger")
             return redirect ("/add")
@@ -38,61 +87,22 @@ def add():
         flash("Added! Thank you!", "success")
         return redirect ("/")
 
-@app.route("/", methods= ["GET", "POST"])
-def showData():
-    if request.method == "GET":
-        return render_template ("showData.html", people={})
-    else:
-        bloodType = request.form ["bloodType"]
-        zipcode = request.form ["zipcode"]
-        name = ""
-        #persons = mongo.db.newrecords.find({"bloodType":bloodType, "zipcode": zipcode})
-        persons = mongo.db.newrecords.aggregate([ {"$match":{"bloodType":bloodType, "zipcode": zipcode}}, {"$sample":{ "size": 10 }} ])
-        print(persons)
-        people =  {}
-        for person in persons:
-            if "name" in person:
-                name = person["name"]
-            people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
-            #print (person, people)"""
-        if people == {}:
-            flash("No applicable donors near you ", "warning")
-        return render_template ("showData.html", people = people)
-
-@app.route("/changeData", methods= ["GET", "POST"])
-def changeData():
-    if request.method == "GET":
-        return render_template ("changeData.html")
-    else:
-        f_name = request.form ["f_name"]
-        l_name = request.form ["l_name"]
-        dob = request.form ["dob"]
-        phone = request.form ["phone"]
-        person = mongo.db.newrecords.find_one({"f_name":f_name, "l_name":l_name, "dob":dob,})
-        #print (person)
-        if person != None:
-            mongo.db.newrecords.update_one({"f_name":f_name, "l_name":l_name, "dob":dob}, {"$set":{"phone":phone}})
-            flash("Updated!", "success")
-            return redirect ("/")
-        flash("didn't work", "warning")
-        return redirect ("/changeData")
 
 @app.route("/deleteUser", methods= ["GET", "POST"])
 def deleteUser():
     if request.method == "GET":
         return render_template ("deleteUser.html")
     else:
-        f_name = request.form ["f_name"]
-        l_name = request.form ["l_name"]
-        dob = request.form ["dob"]
-        person = mongo.db.newrecords.find_one({"f_name":f_name, "l_name":l_name, "dob":dob,})
-        #print (person)
+        name = request.form ["name"].strip()
+        phone = request.form ["phone"].strip()
+        person = mongo.db.newrecords.find_one({"name":name, "phone": phone})
         if person != None:
-            mongo.db.newrecords.delete_one({"f_name":f_name, "l_name":l_name, "dob":dob})
-            flash( f_name+" removed !", "success")
+            mongo.db.newrecords.delete_one({"name":name, "phone": phone})
+            flash( name+" removed !", "success")
             return redirect ("/")
         flash("didn't work", "warning")
         return redirect ("/deleteUser")
+
 
 @app.route("/autoAddData")
 def autoAddData():
@@ -108,4 +118,4 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-"""geotagging, enter in zipcode, all they see is phone + psuedo-name, check nearest zip codes to fill in 10, search again to get 10, up to 3 sarch agains, after have to wait 10 min, OTP (verification) to register & remove, email for identifier, senf proof of registration to email - sendgrid, info icon, one blood type can donate to others"""
+""" enter in zipcode, all they see is phone + psuedo-name, check nearest zip codes to fill in 10, search again to get 10, up to 3 sarch agains, after have to wait 10 min, OTP (verification) to register & remove, email for identifier, senf proof of registration to email - sendgrid, info icon, one blood type can donate to others, show conditions of donating blood, check box to agree to terms&conditions, recorded that they agreed, removing by email, remember zeros in zipcode"""

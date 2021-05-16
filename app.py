c3339cff5f66432a20a107f0c7c8f43ed1b57877
A. Sharma <ankagugu@gmail.com>
2021-05-15 18:08:46 -0700

    search by city w/ google + minor ui fixes

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ app.config.update(
     SECRET_KEY= os.environ.get("SECRET_KEY"),
     SENDGRID_API= os.environ.get("SENDGRID_API"),
     DOMAIN= os.environ.get("DOMAIN"),
-    SECURITY_PASS_SALT= os.environ.get("SECURITY_PASS_SALT")
+    SECURITY_PASS_SALT= os.environ.get("SECURITY_PASS_SALT"),
+    PLACES_API= os.environ.get("PLACES_API")
 )
 
 
@@ -31,20 +32,16 @@ regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$' #for sever-side validation
 @app.route("/", methods= ["GET", "POST"])
 def showData():
     if request.method == "GET":
-        return render_template ("showData.html", people={})
+        return render_template ("showData.html", people={}, places=app.config['PLACES_API'])
     else:
         bloodType = request.form ["bloodType"]
-        zipcode = request.form ["zipcode"].strip()
+        place = request.form ["place"].strip()
         button = request.form["button"]
-        print (button)
-        if not zipcode.isnumeric():
-            flash("zipcode must be numeric", "warning")
-            return redirect ("/")
 
         #make sure to retain leading zeros in zip when adding and subtracting 1
-        lenZip = len(zipcode)
+        """lenZip = len(zipcode)
         searchZipcodeUP = ( str( int(zipcode) + 1 ) ).zfill(lenZip)
-        searchZipcodeDOWN = ( str( int(zipcode) - 1 ) ).zfill(lenZip)
+        searchZipcodeDOWN = ( str( int(zipcode) - 1 ) ).zfill(lenZip)"""
 
         possibleTypes={"AB+":["O-", "O+", "B-", "B+", "A-", "A+", "AB-", "AB+"],
         "AB-":["O-", "B-", "A-", "AB-"],
@@ -57,23 +54,22 @@ def showData():
         donorTypes=[]
         for donorType in possibleTypes[bloodType]:
             donorTypes.append({"bloodType":donorType})
-            print (donorTypes)
             #db.inventory.find( { $or: [ { quantity: 20 } , { price: 10 } ] } )
         
         name = ""
         people =  {}
         #persons = mongo.db.donors.find({"bloodType":bloodType, "zipcode": zipcode})
-        persons = mongo.db.donors.aggregate([ {"$match": { "$or": donorTypes , "zipcode": zipcode, "confirmed": True}}, {"$sample":{ "size": 10 }} ])
+        persons = mongo.db.donors.aggregate([ {"$match": { "$or": donorTypes , "place": place, "confirmed": True}}, {"$sample":{ "size": 10 }} ])
         if button == "Search Plasma Donors":
-            persons = mongo.db.donors.aggregate([ {"$match":{"$or": donorTypes, "zipcode": zipcode, "confirmed": True, "plasma": True}}, {"$sample":{ "size": 10 }} ])
+            persons = mongo.db.donors.aggregate([ {"$match":{"$or": donorTypes, "place": place, "confirmed": True, "plasma": True}}, {"$sample":{ "size": 10 }} ])
         #print(persons)
         for person in persons:
             if "name" in person:
                 name = person["name"]
-            people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
+            people [person["_id"]] = [name, person["phone"], person["place"], person["bloodType"]]
             #print (person, people)"""
 
-        if len(people) < 10:
+        """if len(people) < 10:
             persons = mongo.db.donors.aggregate([ {"$match":{"$or": donorTypes, "zipcode": searchZipcodeUP, "confirmed": True}}, {"$sample":{ "size": 10 - len(people) }} ])
             if button == "Search Plasma Donors":
                 persons = mongo.db.donors.aggregate([ {"$match":{"$or": donorTypes, "zipcode": searchZipcodeUP, "confirmed": True, "plasma": True}}, {"$sample":{ "size": 10 }} ])
@@ -89,26 +85,26 @@ def showData():
             for person in persons:
                 if "name" in person:
                     name = person["name"]
-                people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]
+                people [person["_id"]] = [name, person["phone"], person["zipcode"], person["bloodType"]]"""
 
         if people == {}:
             flash("No applicable donors near you", "warning")
-        elif len(people) < 10:
+        """elif len(people) < 10:
             zipcodes = zipcode + ", " + searchZipcodeDOWN + ", " + searchZipcodeUP
-            flash("Unable to find 10 donors in "+zipcodes, "warning")
-        return render_template ("showData.html", people = people)
+            flash("Unable to find 10 donors in "+zipcode, "warning")"""
+        return render_template ("showData.html", people = people, places=app.config['PLACES_API'])
 
 
 @app.route("/add", methods= ["GET", "POST"])
 def add():
     if request.method == "GET":
-        return render_template ("add.html")
+        return render_template ("add.html", places=app.config['PLACES_API'])
     else:
-        name = request.form ["name"].strip()
+        name = request.form ["name"].strip().lower()
         phone = request.form ["phone"].strip()
-        zipcode = request.form ["zipcode"].strip()
+        place = request.form ["place"].strip()
         bloodType = request.form ["bloodType"]
-        email = request.form ["email"].strip()
+        email = request.form ["email"].strip().lower()
         plasma = False
         if "plasma" in request.form:
             plasma = True
@@ -118,16 +114,16 @@ def add():
         else:
             flash("Please agree to terms and conditions if you wish to register", "danger")
             return redirect ("/add")
-        if (phone == "" or zipcode == "" or bloodType == ""):
+        if (phone == "" or place == "" or bloodType == ""):
             flash("Please fill in all fields", "danger")
             return redirect ("/add")
         elif not (re.search(regex, email)):
             flash("Please enter valid email", "danger")
             return redirect ("/add")
-        elif not ( zipcode.isnumeric() and phone.isnumeric() ):
-            flash("Please enter numeric values for phone and zipcode", "danger")
+        elif not ( phone.isnumeric() ):
+            flash("Please enter numeric values for phone", "danger")
             return redirect ("/add")
-        person = {"name":name, "phone": phone, "zipcode": zipcode, "bloodType": bloodType, "email": email, "confirmed": False, "agreed_TandC":agree, "plasma":plasma}
+        person = {"name":name, "phone": phone, "place": place, "bloodType": bloodType, "email": email, "confirmed": False, "agreed_TandC":agree, "plasma":plasma}
         #print (person)
         mongo.db.donors.insert_one(person)
 
@@ -137,17 +133,16 @@ def add():
         url = domain + "/email_verification?token=" + token
         
         message = Mail(
-            from_email='donotreply@vitalrelation.com',
-            to_emails='ankagugu@gmail.com',
-            subject='Vital Realtion - Account Confirmation',
-            html_content= render_template("createUser_email.html", name = name, phone = phone, zipcode = zipcode, bloodType = bloodType, url = url))
+            from_email='ankagugu@gmail.com',
+            to_emails= email,
+            subject='Vital Relation - Account Confirmation',
+            html_content= render_template("createUser_email.html", name = name.title(), phone = phone, place = place, bloodType = bloodType, url = url))
         try:
             sg = SendGridAPIClient(app.config['SENDGRID_API'])
             response = sg.send(message)
+            flash("Please check your inbox to confirm your account. Thank you!", "success")
         except Exception as e:
-            print(e.message)
-
-        flash("Please check your inbox to confirm your account. Thank you!", "success")
+            print(e)
         return redirect ("/")
 
 
@@ -186,15 +181,15 @@ def otherResource():
 @app.route("/autoAddData")
 def autoAddData():
     for x in range (5):
-        person = {"name":"TEST", "phone": "123"+str(x), "zipcode": "0010", "bloodType": "A+","email": "test"+str(x)+"@anika.com", "confirmed": True }
+        person = {"name":"TEST", "phone": "123"+str(x), "place": "0010", "bloodType": "A+","email": "test"+str(x)+"@anika.com", "confirmed": True }
         #print (person)
         mongo.db.donors.insert_one(person)
     for x in range (5):
-        person = {"name":"TEST", "phone": "122"+str(x), "zipcode": "0011", "bloodType": "A-","email": "test1"+str(x)+"@anika.com", "confirmed": True }
+        person = {"name":"TEST", "phone": "122"+str(x), "place": "0011", "bloodType": "A-","email": "test1"+str(x)+"@anika.com", "confirmed": True }
         #print (person)
         mongo.db.donors.insert_one(person)
     for x in range (5):
-        person = {"name":"TEST", "phone": "121"+str(x), "zipcode": "0009", "bloodType": "O-","email": "test2"+str(x)+"@anika.com", "confirmed": True }
+        person = {"name":"TEST", "phone": "121"+str(x), "place": "0009", "bloodType": "O-","email": "test2"+str(x)+"@anika.com", "confirmed": True }
         #print (person)
         mongo.db.donors.insert_one(person)
     flash("Added! Thank you!", "success")
@@ -205,10 +200,9 @@ if __name__ == "__main__":
     app.run()
 
 
-""" enter in zipcode, all they see is phone + psuedo-name, check nearest zip codes to fill in 10, search again to get 10, up to 3 sarch agains, after have to wait 10 min, OTP (verification) to register & remove, email for identifier, senf proof of registration to email - sendgrid, info icon, one blood type can donate to others, show conditions of donating blood, check box to agree to terms&conditions, recorded that they agreed, removing by email, remember zeros in zipcode, pagination"""
-"""serializer flask token (url timed sereailizer), token should have email, dotenv """
-
-"""make the email prettier, serializer flask token, confirmed key in record, secret key becomes config variable, .env"""
+"""up to 3 sarch agains, after have to wait 10 min, info icon"""
+#remove debug, change recieving email, from email
+"""make the email prettier"""
 """confirm email to delete user"""
 
 """https://covidwin.in/               Tracking availability of many types of Covid related resources.
